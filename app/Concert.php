cdb1cae3312601221934f15cdb8af6ea28523e4e
Stu Heiss <sheiss@highlandsolutions.com>
2017-01-15 19:04:45 -0600

    Unit Testing Presentation Logic

--- app/Concert.php
+++ app/Concert.php
@@ -9,4 +9,9 @@ class Concert extends Model
     // don't guard any fields from mass assignment
     protected $guarded = [];
     protected $dates = ['date'];
+
+    public function getFormattedDateAttribute()
+    {
+        return $this->date->format('F j, Y');
+    }
 }
